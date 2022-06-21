cf7c6da3b529effc8102447f78d586fe8b731b52
Boris Garkov <boris.garkov@abv.bg>
2022-06-21 23:50:02 +0300

    main page updated + images resized

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,88 +1,195 @@
-import { Stack, Typography, Button, FormControl, InputLabel, Select, MenuList } from "@mui/material";
-import logo from '../images/Rent A Hand_D1.png';
+import { Stack, Typography, Button, FormControl, InputLabel, Select, MenuList, Card, CardContent, Paper, Tabs, Tab } from "@mui/material";
+import job_offers_img from '../images/main_page/main_page_job_offers.jpeg';
+import projects_img from '../images/main_page/main_page_projects.jpeg';
+import freelancer_img from '../images/main_page/main_page_freelancer.jpeg';
 
-import { work_categories, work_type } from '../JobsPage/work-categories';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ApartmentOutlinedIcon from '@mui/icons-material/ApartmentOutlined';
+import WorkIcon from '@mui/icons-material/Work';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+
 import useScreenResolution from "../hooks/useScreenResolution";
 
 import styles from './main-page.module.css'
+import { useState } from "react";
+
+const tabs_css_style = {
+    padding: {
+        xs: '0 10px',
+        md: '0 50px',
+    },
+    // fontWeight: 'bold',
+    // textAlign: 'justify',
+};
+
+const JobsTabInfo = () => {
+    return (
+        <>
+            <Stack sx={{
+                gap: 2,
+                justifyContent: 'center',
+                marginBottom: 5,
+            }}>
+                <WorkIcon fontSize='large' sx={{ alignSelf: 'center', color: 'primary.main' }} />
+                <Typography variant='h5' sx={{ ...tabs_css_style }}>
+                    Предлагате работа?
+                </Typography>
+                <Typography variant='body1' sx={{ ...tabs_css_style }}>
+                    Споделете вашата обява за работа / стаж и намерете подходящия кандидат.
+                </Typography>
+                <Typography variant='body1' sx={{ marginBottom: 2.5, ...tabs_css_style }}>
+                    Научете повече за предимствата на нашата платформа
+                </Typography>
+            </Stack>
+
+
+            <Stack sx={{
+                flexDirection: { md: 'row' },
+                justifyContent: 'space-evenly',
+                marginBottom: 5,
+            }}>
+                <Button variant='contained'>Корпоративни Планове</Button>
+                <Button variant='outlined'>Обяви за работа</Button>
+            </Stack>
+        </>
+    )
+};
+
+const ProjectsTabInfo = () => {
+    return (
+        <>
+            <Stack sx={{
+                gap: 2,
+                justifyContent: 'center',
+                marginBottom: 5,
+            }}>
+                <AssignmentIcon fontSize='large' sx={{ alignSelf: 'center', color: 'primary.main' }} />
+                <Typography variant='h5' sx={{ ...tabs_css_style }}>
+                    Имаш проект / задача, за който не се изисква дългосрочен трудов договор?
+                </Typography>
+                <Typography variant='body1' sx={{ marginBottom: 2.5, ...tabs_css_style }}>
+                    Намери подходящ талант в нашата платформа, за да свърши нужната работа.
+                </Typography>
+            </Stack>
+            <Stack sx={{
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                marginBottom: 5,
+            }}>
+                <Button variant='contained'>Към регистрация</Button>
+                <Button variant='outlined'>Проекти</Button>
+            </Stack>
+        </>
+    )
+};
+
+const FreelancerTabInfo = () => {
+    return (
+        <>
+            <Stack sx={{
+                gap: 2,
+                justifyContent: 'center',
+                marginBottom: 5,
+            }}>
+                <StarBorderIcon fontSize='large' sx={{ alignSelf: 'center', color: 'primary.main' }} />
+                <Typography variant='h5' sx={{ ...tabs_css_style }}>
+                    Искаш да работиш за себе си?
+                </Typography>
+                <Typography variant='body1' sx={{ ...tabs_css_style }}>
+                    Работи в удобно за теб време, където и когато поискаш.
+                </Typography>
+                <Typography variant='body1' sx={{ marginBottom: 2.5, ...tabs_css_style }}>
+                    Намери подходящия проект за твоите умения,
+                    като споделиш опита и знанията си в нашата платформа.
+                </Typography>
+            </Stack>
+            <Stack sx={{
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                marginBottom: 5,
+            }}>
+                <Button variant='contained'>Към регистрация</Button>
+            </Stack>
+        </>
+    )
+};
 
 export default function MainPage() {
+
+    const [tab, setTab] = useState(0);
+    const [backgroundImg, setBackgroundImg] = useState(job_offers_img);
+
+    const tab_images = {
+        0: job_offers_img,
+        1: projects_img,
+        2: freelancer_img,
+    };
+
     const isModileDevice = useScreenResolution();
+    const tabsChangeHandler = (event, newTab) => {
+        setTab(newTab);
+        setBackgroundImg(tab_images[newTab]);
+    };
+
+    const tabs_info = [
+        {
+            label: 'Обяви за работа',
+            icon: <WorkIcon />,
+            value: 0
+        },
+        {
+            label: 'Проекти / Задачи',
+            icon: <AssignmentIcon />,
+            value: 1
+        },
+        {
+            label: 'Таланти',
+            icon: <StarBorderIcon />,
+            value: 2
+        },
+    ];
+
+    const tabs_data = {
+        0: <JobsTabInfo />,
+        1: <ProjectsTabInfo />,
+        2: <FreelancerTabInfo />,
+    };
 
     return (
         <>
-            <Stack
-                sx={{
-                    flexDirection: {
-                        xs: 'column',
-                        sm: 'row',
-                    },
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    gap: 10,
-                }}
-            >
 
-                {
-                    isModileDevice ?
-                        '' :
-                        <img src={logo} alt='logo' className={styles['image']} />
-                }
-
-                <Stack>
-                    <Stack sx={{
-                        marginTop: {
-                            xs: 5,
-                            sm: 10,
-                        },
-                        textAlign: 'center',
-                    }}>
-                        <Typography variant='h4'>
-                            ПЛАТФОРМА ЗА РАБОТА
-                        </Typography>
-                        <Typography variant='h5'>
-                            Сподели обява с няколко клика
-                        </Typography>
-
-                    </Stack>
-                    <Stack sx={{
-                        marginTop: 5,
-                        textAlign: 'center',
-                    }}>
-                        <Button variant='contained'>Създай обява</Button>
-                        <Typography variant='h5' sx={{ marginTop: 3 }}>
-                            Потърси обяви
-                        </Typography>
-                        <FormControl
-                            variant='standard'
-                            sx={{ m: 1 }}
-                        >
-                            <InputLabel>Категория</InputLabel>
-                            <Select>
-                                {
-                                    work_categories.map(work => (
-                                        <MenuList key={work} value={work}>{work}</MenuList>
-                                    ))
-                                }
-                            </Select>
-                        </FormControl>
-                        <FormControl
-                            variant='standard'
-                            sx={{ m: 1 }}
-                        >
-                            <InputLabel>Тип работа</InputLabel>
-                            <Select>
-                                {
-                                    work_type.map(work => (
-                                        <MenuList key={work} value={work}>{work}</MenuList>
-                                    ))
-                                }
-                            </Select>
-                        </FormControl>
-                        <Button variant='contained'>Търси</Button>
-                    </Stack>
-                </Stack>
+            <Stack>
+                <img className={styles['image']} loading='lazy' src={backgroundImg} alt='job offers img' />
             </Stack>
+            <Paper sx={{ width: { xs: '100%', md: '35%' }, position: { xs: 'static', md: 'absolute' }, top: { md: '20%' }, left: { md: '10%' } }}>
+                <Tabs
+                    value={tab}
+                    centered
+                    variant='fullWidth'
+                    sx={{
+                        marginBottom: 5
+                    }}
+                    onChange={tabsChangeHandler}
+                >
+                    {
+                        tabs_info.map(x => (
+                            <Tab
+                                key={x.label}
+                                wrapped
+                                label={x.label}
+                                icon={x.icon}
+                                iconPosition='top'
+                                value={x.value}
+                                sx={tabs_css_style}
+                            />
+                        ))
+                    }
+                </Tabs>
+                {
+                    tabs_data[tab]
+                }
+            </Paper>
         </>
     )
 }
