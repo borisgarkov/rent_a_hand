b32ce0e0b411b59d28bb7ef682e31f8b6e1024d0
Boris Garkov <boris.garkov@abv.bg>
2022-07-23 22:35:38 +0300

    jobs page design update

--- src/components/JobsPage/JobPreviewDialog.js
+++ src/components/JobsPage/JobPreviewDialog.js
@@ -8,7 +8,7 @@ import Slide from '@mui/material/Slide';
 import { Avatar, Box, IconButton, Typography } from '@mui/material';
 
 import CloseIcon from '@mui/icons-material/Close';
-import { forwardRef, useState } from 'react';
+import { forwardRef } from 'react';
 
 import { useNavigate } from "react-router-dom";
 
@@ -16,60 +16,47 @@ const Transition = forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function JobPreviewDialog({ job }) {
-    const [open, setOpen] = useState(false);
+export default function JobPreviewDialog({ job, handleClose, open }) {
+
     let navigate = useNavigate();
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
     return (
-        <div>
-            <Button variant="text" onClick={handleClickOpen}>
-                Научи повече
-            </Button>
-            <Dialog
-                open={open}
-                TransitionComponent={Transition}
-                keepMounted
-                onClose={handleClose}
-                aria-describedby="alert-dialog-slide-description"
-            >
-                <DialogTitle>
-                    <Box
-                        marginBottom={2}
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'baseline',
-                            columnGap: 2,
-                        }}
-                    >
-                        <Avatar>{job.publisher.slice(0, 1)}</Avatar>
-                        <Typography id="transition-modal-title" variant="h6" component="h2" mb={2}>
-                            {job.publisher}
-                        </Typography>
-                        <IconButton onClick={handleClose} sx={{ alignSelf: 'flex-start', marginLeft: 'auto' }} >
-                            <CloseIcon />
-                        </IconButton>
-                    </Box>
-                    {job.title}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-slide-description">
-                        {job.description}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => (navigate('/job-description'))}>
-                        Прегледай обява
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div >
+        <Dialog
+            open={open}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleClose}
+            aria-describedby="alert-dialog-slide-description"
+        >
+            <DialogTitle>
+                <Box
+                    marginBottom={2}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'baseline',
+                        columnGap: 2,
+                    }}
+                >
+                    <Avatar>{job.publisher.slice(0, 1)}</Avatar>
+                    <Typography id="transition-modal-title" variant="h6" component="h2" mb={2}>
+                        {job.publisher}
+                    </Typography>
+                    <IconButton onClick={handleClose} sx={{ alignSelf: 'flex-start', marginLeft: 'auto' }} >
+                        <CloseIcon />
+                    </IconButton>
+                </Box>
+                {job.title}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    {job.description}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => (navigate('/job-description'))}>
+                    Прегледай обява
+                </Button>
+            </DialogActions>
+        </Dialog>
     );
 }
--- src/components/JobsPage/JobsCatalog.js
+++ src/components/JobsPage/JobsCatalog.js
@@ -2,43 +2,104 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
-import { Avatar, CardActions, CardMedia, Typography } from '@mui/material';
+import { Avatar, Box, Button, CardActionArea, Divider, IconButton, Stack, Typography } from '@mui/material';
 import JobPreviewDialog from './JobPreviewDialog';
+import { useState } from 'react';
+
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+import BookmarkAddedIcon from '@mui/icons-material/BookmarkAdded';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import HomeRepairServiceIcon from '@mui/icons-material/HomeRepairService';
+import WorkIcon from '@mui/icons-material/Work';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+
+import logo from '../images/Rent A Hand_D1.png';
+import style from '../ProfilePage/profile-page-style.module.css';
+import { fontSize } from '@mui/system';
 
 
 export default function JobsCatalog({
     job
 }) {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <Grid item xs='auto' key={job.id} sx={{
+        <Grid item xs={12} key={job.id} sx={{
             margin: {
                 xs: 'auto',
                 sm: 0,
             }
         }}>
-            <Card sx={{ maxWidth: 320 }}>
-                <CardHeader
-                    title={job.title}
-                    subheader={job.datePosted}
-                    avatar={
-                        <Avatar>{job.publisher.slice(0, 1)}</Avatar>
-                    }
-                />
-                <CardMedia
-                    component='img'
-                    height='200'
-                    image='https://source.unsplash.com/random'
-                    alt='card img'
-                />
-                <CardContent>
-                    <Typography variant='body2' color='text.secondary'>
-                        {job.description.slice(0, 150)}...
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <JobPreviewDialog job={job} />
-                </CardActions>
-            </Card>
-        </Grid>
+            <Card sx={{
+                display: 'flex',
+            }}>
+                <Stack sx={{ width: { xs: '40%', sm: '30%' } }}>
+                    <CardHeader
+                        title='Rent A Hand'
+                        subheader='ИТ'
+                        avatar={<Avatar src={logo} className={style['image-style']} />}
+                        titleTypographyProps={{ fontSize: { sm: '1rem' }, fontWeight: 'bold', }}
+                        subheaderTypographyProps={{ fontSize: { sm: '0.875rem' } }}
+                        sx={{ flexDirection: { xs: 'column', sm: 'row' }, padding: { xs: 0, sm: 2 } }}
+                    />
+                    <CardContent sx={{
+                        display: 'flex',
+                        gap: 2,
+                        flexDirection: { xs: 'column', sm: 'row' },
+                        paddingTop: { xs: 0, sm: 2 },
+                        paddingBottom: { xs: 0, sm: 2 },
+                    }}>
+                        <Stack sx={{ flexDirection: 'row', alignItems: 'center', cursor: 'pointer', gap: 1 }}>
+                            <WorkIcon sx={{ color: 'primary.main' }} fontSize='small' />
+                            <Typography variant='body2'>Всички обяви</Typography>
+                        </Stack>
+                        <Stack sx={{ flexDirection: 'row', alignItems: 'center', cursor: 'pointer', gap: 1 }}>
+                            <PersonOutlineIcon sx={{ color: 'primary.main' }} fontSize='small' />
+                            <Typography variant='body2'>Профил</Typography>
+                        </Stack>
+                    </CardContent>
+                </Stack>
+                <Stack sx={{
+                    flexDirection: 'row',
+                    alignItems: 'flex-start',
+                    width: '70%',
+                }}>
+                    <Divider orientation='vertical' flexItem />
+                    <Box>
+                        <CardHeader
+                            title={job.title}
+                            subheader={job.datePosted}
+                            titleTypographyProps={{ fontSize: { xs: '0.875rem', sm: '1rem' }, fontWeight: 'bold', textOverflow: '' }}
+                            subheaderTypographyProps={{ fontSize: '0.875rem' }}
+                            sx={{ paddingBottom: { xs: 0, sm: 2 } }}
+                        />
+                        <CardContent sx={{
+                            display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, gap: { xs: 1, sm: 5 },
+                        }}>
+                            <Stack sx={{ flexDirection: 'row', gap: 1 }}>
+                                <LocationOnIcon sx={{ color: 'primary.main' }} fontSize='small' />
+                                <Typography variant='body2'>{job.city}</Typography>
+                            </Stack>
+                            <Stack sx={{ flexDirection: 'row', gap: 1 }}>
+                                <HomeRepairServiceIcon sx={{ color: 'primary.main' }} fontSize='small' />
+                                <Typography variant='body2'>{job.jobCategory}</Typography>
+                            </Stack>
+                        </CardContent>
+                    </Box>
+                    <IconButton sx={{ marginLeft: 'auto' }}>
+                        <BookmarkAddedIcon sx={{ color: '#f37327' }} />
+                    </IconButton>
+                </Stack>
+                {/* <JobPreviewDialog job={job} handleClose={handleClose} open={open} /> */}
+            </Card >
+        </Grid >
     )
 }
